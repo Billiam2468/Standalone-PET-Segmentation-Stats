bea098fad549e1927ecfb01ae07cecc74b622896
Billiam2468 <kirimlee1@gmail.com>
2025-01-02 10:32:28 -0800

    Created updated segmentationStats executable

--- segmentationStats.py
+++ segmentationStats.py
@@ -680,8 +680,8 @@ def statistics_from_rois(segmentation_dir, pet_dir, name_reference):
 
                 # If segmentations already made with different naming conventions, have to get the right PET name
                 pet_name = segmentation.name.removesuffix(seg_extension)
-                pet_name = pet_name.removeprefix("joints_")
-                pet_name = pet_name.removesuffix("_FLU90")
+                # pet_name = pet_name.removeprefix("joints_")
+                # pet_name = pet_name.removesuffix("_FLU90")
 
                 seg_dir = os.path.join(segmentation_dir, segmentation.name)
 
