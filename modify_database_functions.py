e4eaf85a77492236101bf0d5962efc9a66a40347
Billiam2468 <kirimlee1@gmail.com>
2025-04-06 15:08:21 -0700

    modify database functions implemented

--- modify_database_functions.py
+++ modify_database_functions.py
@@ -3,17 +3,9 @@
 import os
 import shutil
 
-# def date_subfolders_to_patient_date_name(home_dir):
-#     with os.scandir(home_dir) as patients:
-#         for patient in patients:
-#             if patient.is_dir():
-#                 patient_dir = os.path.join(home_dir, patient)
-#                 with os.scandir(patient_dir) as dates:
-#                     for date in dates:
-#                         if date.is_dir():
-#                             print(date.name)
 
-
+# This function reorganizes a home directory of patient scans that have date subfolders inside rather than PET and CT folders directly inside the patient folder
+# It will reorganize the database to remove the date subfolder and rename the patient folder instead with PET and CT folders directly inside
 def date_subfolders_to_patient_date_name(home_dir):
     with os.scandir(home_dir) as patients:
         for patient in patients:
@@ -55,4 +47,30 @@ def date_subfolders_to_patient_date_name(home_dir):
                             shutil.rmtree(os.path.join(patient_dir, f))
                     os.rmdir(patient_dir)
 
-date_subfolders_to_patient_date_name("/Users/williamlee/Documents/Example/test copy")
+#date_subfolders_to_patient_date_name("/Users/williamlee/Documents/Example/test copy")
+
+# This function takes the current names of the CT and PET folders in a patient directory and renames it to the standard CT and PET
+def rename_to_PET_and_CT(home_dir, ct_folder, pet_folder):
+    with os.scandir(home_dir) as patients:
+        for patient in patients:
+            if patient.is_dir():
+                patient_name = patient.name
+                patient_dir = os.path.join(home_dir, patient_name)
+
+                with os.scandir(patient_dir) as scans:
+                    for scan in scans:
+                        if scan.is_dir():
+                            print(scan.name)
+                            old_scan_path = os.path.join(patient_dir, scan.name)
+
+                            if scan.name == ct_folder:
+                                new_scan_path = os.path.join(patient_dir, "CT")
+                                os.rename(old_scan_path, new_scan_path)
+                                print(f"Renamed {scan.name} to CT in {patient_name}")
+
+                            elif scan.name == pet_folder:
+                                new_scan_path = os.path.join(patient_dir, "PET")
+                                os.rename(old_scan_path, new_scan_path)
+                                print(f"Renamed {scan.name} to PET in {patient_name}")
+
+#rename_to_PET_and_CT("/Users/williamlee/Documents/Example/test copy 2", "old_CT", "old_PET")
